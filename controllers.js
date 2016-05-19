83c19837c2e7309445a3170da7ed9bb038b144eb
norbson <norbert.krzysztofik@4finance.com>
2016-05-19 17:42:42 +0200

    add tabs

--- controllers.js
+++ controllers.js
@@ -1,4 +1,20 @@
 app
-    .controller('StoreController', function (Gems) {
-    	this.products = Gems 	  
+    .controller('StoreController', function(Gems) {
+    	this.products = Gems; 	  
     })
+    .controller('PanelController', function() {
+		this.tab = 1;
+		this.selectTab = function(setTab) {
+			this.tab = setTab
+		};
+		this.isSelected = function(isSelectedTab) {
+			return this.tab === isSelectedTab
+		};
+
+    })
+    .controller('GalleryController', function() {
+    	this.current = 0;
+    	this.setCurrent = function(newValue) {
+      		this.current = newValue ? newValue : 0;  
+    	};
+  })
